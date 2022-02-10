a6ce48b918fa74daa65d4dce80b6f1c4f78805e1
saiful-md <mdsaifulislam2491@gmail.comm>
2022-02-10 10:44:24 +0600

    first commit

---
+++ src/Pages/Home/SingleService/SingleService.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleService = () => {
+  return (
+    <div>
+      <h1>this is single service page...</h1>
+    </div>
+  );
+};
+
+export default SingleService;
