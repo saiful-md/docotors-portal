16b51be3a21d747d00b2909e504c146dd08ab71b
saiful-md <mdsaifulislam2491@gmail.comm>
2022-02-19 12:27:07 +0600

    added observer of firebase

--- src/App.js
+++ src/App.js
@@ -5,35 +5,38 @@ import Login from "./Pages/Registration/Login/Login";
 import Register from "./Pages/Registration/Registar/Register";
 import Appointment from "./Appointment/Appoiontment/Appointment";
 import DashBoard from "./Pages/DashBoard/DashBoard/DashBoard";
+import AuthProvider from "./Contexts/AuthProvider/AuthProvider";
 
 function App() {
   return (
     <div className='App'>
-      <Router>
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/home'>
-            <Home />
-          </Route>
-          <Route path='/appointment'>
-            <Appointment />
-          </Route>
-          <Route path='/dashboard'>
-            <DashBoard />
-          </Route>
-          <Route path='/login'>
-            <Login />
-          </Route>
-          <Route path='/register'>
-            <Register />
-          </Route>
-          <Route path='*'>
-            <h1>Nothing page available..</h1>
-          </Route>
-        </Switch>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/home'>
+              <Home />
+            </Route>
+            <Route path='/appointment'>
+              <Appointment />
+            </Route>
+            <Route path='/dashboard'>
+              <DashBoard />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/register'>
+              <Register />
+            </Route>
+            <Route path='*'>
+              <h1>Nothing page available..</h1>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
